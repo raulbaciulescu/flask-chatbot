c34e00415b302ede7e2abbd5f5e12863c02bd73d
raulbaciulescu <raulbaciulescu@gmail.com>
2023-06-08 23:31:24 +0300

    checkpoint

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import os
+import sys
+import time
 
-from flask import Flask, request, jsonify
 import openai
+import pinecone
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from langchain import OpenAI, ConversationChain
 from langchain.chains.question_answering import load_qa_chain
-from langchain.memory import ConversationSummaryBufferMemory
-import sys
-from flask_cors import CORS, cross_origin
-from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, UnstructuredFileLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from utils import recognize, audio_directory, pdf_directory, write_to_file, should_create_index, \
-    texts_directory
-from langchain.vectorstores import Chroma, Pinecone
+from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
-import pinecone
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Pinecone
+
+from utils import recognize, audio_directory, pdf_directory, write_to_file, should_create_index
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -86,6 +87,8 @@ def transcribe():
 @app.route('/pdf-messages', methods=['POST'])
 @cross_origin()
 def create_pdf_file():
+    start_global = time.perf_counter()
+    start = time.perf_counter()
     if 'file' not in request.files:
         return 'No file found in request!', 400
 
@@ -99,19 +102,29 @@ def create_pdf_file():
     data = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     texts = text_splitter.split_documents(data)
+    finish = time.perf_counter()
+    print(f'Finished in {round(finish - start, 2)} seconds(s), load data')
+    start = time.perf_counter()
     if len(pinecone.list_indexes()) > 0:
         pinecone.delete_index(index_name)
     pinecone.create_index(index_name, dimension=1536)
     docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
-
+    finish = time.perf_counter()
+    print(f'Finished in {round(finish - start, 2)} seconds(s), delete/create index')
+    start = time.perf_counter()
     docs = docsearch.similarity_search(message, include_metadata=True)
     text = chain.run(input_documents=docs, question=message)
+    finish = time.perf_counter()
+    print(f'Finished in {round(finish - start, 2)} seconds(s), similarity and chain run')
+    finish_global = time.perf_counter()
+    print(f'Finished in {round(finish_global - start_global, 2)} seconds(s)')
     return jsonify({'text': text})
 
 
 @app.route('/pdf-messages/without-pdf', methods=['POST'])
 @cross_origin()
 def create_pdf_message():
+    start_global = time.perf_counter()
     # save file
     message = request.get_json()['message']
     filename = request.get_json()['filename']
@@ -128,6 +141,8 @@ def create_pdf_message():
     text = chain.run(input_documents=docs, question=message)
 
     write_to_file(filename)
+    finish_global = time.perf_counter()
+    print(f'Finished in {round(finish_global - start_global, 2)} seconds(s)')
     return jsonify({'text': text})
 
 
